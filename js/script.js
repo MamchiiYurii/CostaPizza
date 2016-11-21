3363e37f2bfc36b4cf1e0549cee562ba48c9afa7
MamchiiYurii <y.mamchiy@gmail.com>
2016-11-22 00:38:05 +0200

    Parallax

--- js/script.js
+++ js/script.js
@@ -1,134 +1,7 @@
-//--------------------------------Paralax for main page-----------------------------
-/*$(document).ready(
-    function() {
-        dotNav(); // викликаємо функцію одразу, щоб як тілкьи загружався док. вона вже могла викор.
-        $(window).bind("scroll", function(event) {
-            parallaxScroll();
-            dotNav();
-        })
-
-
-
-        $("nav#dots a").hover( // функція вспливання секції
-            function() {
-                $(this).prev("h1").show()
-            },
-            function() {
-                $(this).prev("h1").hide()
-            }
-        )
-    }
-)
-
-function parallaxScroll() { // розраховуємо свій час для кожного з елементів для ефекту паралакс! Це ОСНОВНЕ!!!
-    var scrolled = $(window).scrollTop();
-    $("#parallax-bg1").css("top", (0 - (scrolled * 0.25)) + "px");
-    $("#parallax-bg2").css("top", (0 - (scrolled * 0.5)) + "px");
-    $("#parallax-bg3").css("top", (0 - (scrolled * 0.75)) + "px");
-}*/
-
-//--------------------------------menu pages-----------------------------
-function addPage(page, book) {
-
-    var id, pages = book.turn('pages');
-
-    // Create a new element for this page
-    var element = $('<div />', {});
-
-    // Add the page to the flipbook
-    if (book.turn('addPage', element, page)) {
-
-        // Add the initial HTML
-        // It will contain a loader indicator and a gradient
-        element.html('<div class="gradient"></div><div class="loader"></div>');
-
-        // Load the page
-        loadPage(page, element);
-    }
-
-}
-
-function loadPage(page, pageElement) {
-
-    // Create an image element
-
-    var img = $('<img />');
-
-    img.mousedown(function (e) {
-        e.preventDefault();
-    });
-
-    img.load(function () {
-
-        // Set the size
-        $(this).css({
-            width: '100%',
-            height: '100%'
-        });
-
-        // Add the image to the page after loaded
-
-        $(this).appendTo(pageElement);
-
-        // Remove the loader indicator
-
-        pageElement.find('.loader').remove();
-    });
-
-    // Load the page
-
-    img.attr('src', 'img/menu' + page + '.jpg');
-
-}
-
-
-function loadLargePage(page, pageElement) {
-
-    var img = $('<img />');
-
-    img.load(function () {
-
-        var prevImg = pageElement.find('img');
-        $(this).css({
-            width: '100%',
-            height: '100%'
-        });
-        $(this).appendTo(pageElement);
-        prevImg.remove();
-
-    });
-
-    // Loadnew page
-
-    img.attr('src', 'img/menu' + page + '-large.jpg');
-}
-
-
-function loadSmallPage(page, pageElement) {
-
-    var img = pageElement.find('img');
-
-    img.css({
-        width: '100%',
-        height: '100%'
-    });
-
-    img.unbind('load');
-    // Loadnew page
-
-    img.attr('src', 'img/menu' + page + '.jpg');
-}
-
-
-
-// http://code.google.com/p/chromium/issues/detail?id=128488
-function isChrome() {
-
-    return navigator.userAgent.indexOf('Chrome') != -1;
-
-}
-
+//
 //--------------------------------------------------Gallery-----------------------
+//
+
 //Start slider gallery
 $(document).ready(function () {
     $(".fancybox-thumb")
@@ -169,4 +42,65 @@ $(document).ready(function () {
             }
 
         });
+
 });
+//------- top food menu description-----
+
+
+//var findId = function() {
+//    document.body.onclick = function(event) {
+//         t=event.target||event.srcElement; 
+//        return t.id;
+//    }
+//}
+// console.log(findId);
+//window.onload = function(findId) {
+//$("#"+findId+'"').click(function () {
+//   
+//    if (
+//            $(".descript").is(":hidden")) {
+//            $(".descript").show("slow");
+//
+//            $(".click").val("Згорнути");
+//        
+//    } else {
+//        $(".descript").hide();
+//
+//        $(".click").val("Детальніше");
+//    }
+//});
+//}
+$(document).ready(
+    function () {
+        $(window).bind("scroll", function (event) {
+            parallaxScroll();
+        })
+    })
+
+function parallaxScroll() {
+    var scrolled = $(window).scrollTop();
+
+    $(".scrl").css("top", (0 - (scrolled * 0.09)) + "px");
+    $(".scrl").css("left", (0 - (scrolled * 0.045)) + "px");
+    $(".scrl2").css("top", (0 - (scrolled * 0.09)) + "px");
+    $(".scrl2").css("left", (0 - (scrolled * 0.045)) + "px");
+    $(".scrl3").css("top", (0 - (scrolled * 0.09)) + "px");
+    $(".scrl3").css("left", (0 - (scrolled * 0.05)) + "px");
+    
+    $(".scrl4").css("top", (0 - (scrolled * 0.06)) + "px");
+    $(".scrl4").css("left", (0 - (scrolled * 0.015)) + "px");
+    $(".scrl5").css("top", (0 - (scrolled * 0.06)) + "px");
+    $(".scrl5").css("left", (0 - (scrolled * 0.02)) + "px");
+    $(".scrl6").css("top", (0 - (scrolled * 0.06)) + "px");
+    $(".scrl6").css("left", (0 - (scrolled * 0.02)) + "px");
+    
+    $(".scrl7").css("top", (0 - (scrolled * 0.03)) + "px");
+    $(".scrl7").css("left", (0 + (scrolled * 0.02)) + "px");
+    $(".scrl8").css("top", (0 - (scrolled * 0.03)) + "px");
+    $(".scrl8").css("left", (0 + (scrolled * 0.02)) + "px");
+    $(".scrl9").css("top", (0 - (scrolled * 0.03)) + "px");
+    $(".scrl9").css("left", (0 + (scrolled * 0.02)) + "px");
+    
+    $(".scrl10").css("left", (0 + (scrolled * 0.06)) + "px");
+    $(".scrl11").css("left", (0 - (scrolled * 0.020)) + "px");
+}
