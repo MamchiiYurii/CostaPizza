6b242f438af6e7c700e6251680bfa0e57000001f
MamchiiYurii <y.mamchiy@gmail.com>
2016-11-23 19:18:50 +0200

    rc1

--- js/script.js
+++ js/script.js
@@ -44,32 +44,24 @@ $(document).ready(function () {
         });
 
 });
+
+
+
+
 //------- top food menu description-----
 
+$(".click").click(function () {
+    if ($(this).parents('.ttp').find('.descript').is(":hidden")) {
+        $(this).parents('.ttp').find('.descript').show("slow");
+        $(this).parents('.ttp').find('.click').val("Згорнути");
+    } else {
+        $(this).parents('.ttp').find('.descript').hide("slow");
+        $(this).parents('.ttp').find('.click').val("Детальніше");
+    }
+});
 
-//var findId = function() {
-//    document.body.onclick = function(event) {
-//         t=event.target||event.srcElement; 
-//        return t.id;
-//    }
-//}
-// console.log(findId);
-//window.onload = function(findId) {
-//$("#"+findId+'"').click(function () {
-//   
-//    if (
-//            $(".descript").is(":hidden")) {
-//            $(".descript").show("slow");
-//
-//            $(".click").val("Згорнути");
-//        
-//    } else {
-//        $(".descript").hide();
-//
-//        $(".click").val("Детальніше");
-//    }
-//});
-//}
+
+//--------------------------------- parallax for main page----------------
 $(document).ready(
     function () {
         $(window).bind("scroll", function (event) {
@@ -86,21 +78,21 @@ function parallaxScroll() {
     $(".scrl2").css("left", (0 - (scrolled * 0.045)) + "px");
     $(".scrl3").css("top", (0 - (scrolled * 0.09)) + "px");
     $(".scrl3").css("left", (0 - (scrolled * 0.05)) + "px");
-    
+
     $(".scrl4").css("top", (0 - (scrolled * 0.06)) + "px");
     $(".scrl4").css("left", (0 - (scrolled * 0.015)) + "px");
     $(".scrl5").css("top", (0 - (scrolled * 0.06)) + "px");
     $(".scrl5").css("left", (0 - (scrolled * 0.02)) + "px");
     $(".scrl6").css("top", (0 - (scrolled * 0.06)) + "px");
     $(".scrl6").css("left", (0 - (scrolled * 0.02)) + "px");
-    
+
     $(".scrl7").css("top", (0 - (scrolled * 0.03)) + "px");
     $(".scrl7").css("left", (0 + (scrolled * 0.02)) + "px");
     $(".scrl8").css("top", (0 - (scrolled * 0.03)) + "px");
     $(".scrl8").css("left", (0 + (scrolled * 0.02)) + "px");
     $(".scrl9").css("top", (0 - (scrolled * 0.03)) + "px");
     $(".scrl9").css("left", (0 + (scrolled * 0.02)) + "px");
-    
+
     $(".scrl10").css("left", (0 + (scrolled * 0.06)) + "px");
     $(".scrl11").css("left", (0 - (scrolled * 0.020)) + "px");
 }
